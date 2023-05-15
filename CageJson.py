d293f1683fcda9dbe040c44e8861dee1c952009f
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-05-15 17:59:14 +0000

    json

--- CageJson.py
+++ CageJson.py
@@ -1,6 +1,10 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+from imdb import Cinemagoer
+import imdb.helpers as helpers
+
+ia = Cinemagoer()
 
 
 html = requests.get('https://m.imdb.com/name/nm0000115/fullcredits', timeout=50)
@@ -11,20 +15,20 @@ filmography_html = soup.find(id='filmo-head-actor')
 
 all_films_html = filmography_html.find_next_sibling("div")
 
+movies = []
+
 for div in all_films_html.find_all('div'):
-    title = div.b.a['href']
+    movie = ia.get_movie(div.b.a['href'].split('/')[2].strip('tt'))
+    print(div.b.a.text.strip('\n').strip())
+    movie_dict = {
+        'title' : div.b.a.text.strip('\n').strip(),
+        'year' : div.span.text.strip('\n').strip(),
+        'character' : div.br.next_sibling.strip('\n').strip(),
+        'plot' : movie.get('plot'),
+        'genres' : movie.get('genres'),
+        'cover' : movie.get('cover url')
+    }
+    movies.append(movie_dict)
+print(json.dumps(movies, indent=3))
 
-    title_html = requests.get(f'https://www.imdb.com{title}', timeout=50)
-    title_soup = BeautifulSoup(title_html.text, 'html.parser')
-
-    print(div.b.a.text.strip('\n').strip(), ':', div.span.text.strip('\n').strip(), ':', div.br.next_sibling.strip('\n').strip())
-
-# for child in all_films_html.children:
-#     print(child.text.split('\n'))
-#     print(child.find('a'))
-#     # print(child.text)
-#     print('########################')
-
-# filmography = BeautifulSoup(filmography_html.text, 'html.parser')
-
-# print(filmography.find_all('div'))
+with open('')

89e9a075177b76cb291f6e02872f2a79b356f503
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-05-15 10:04:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 
+fetch('https://m.imdb.com/name/nm0000115/fullcredits').then(function (response) {
+	// The API call was successful!
+	return response.text();
+}).then(function (html) {
+	// This is the HTML from our response as a text string
+	console.log(html);
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});

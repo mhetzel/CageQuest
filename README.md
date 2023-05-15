9a74ad88f5146fbe5fa65948ef623e1c3ae87f1a
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-05-15 19:08:49 +0000

    list

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Cage Quest
 Watch all Nic Cage's Movies
 
+run: python -m http.server
+
+hosted at: https://mhetzel.github.io/CageQuest/
+
 [IMDB](https://m.imdb.com/name/nm0000115/fullcredits)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-
-$.get('https://m.imdb.com/name/nm0000115/fullcredits', function(html) { 
-	console.log(html); 
+$.getJSON('movies.json', function(data) {
+     $.each(data, function(i, f) {
+         console.log(f)
+		 container_div = $('<div class="container"></div>')
+		 if (f['cover']) {
+			container_div.append('<div class="image"><img src="'+ f['cover'] +'" width="50" height="75"></div>')
+		 }
+		 else {
+			container_div.append('<div class="image"><img src="android/comingsoon.jpg" width="50" height="75"></div>')
+		 }
+		 
+		 container_div.append('<div class="text">' + f['title'] + ' (' + f['year'] + ')</br>' + f['character']+'</div>')
+		 $('#unwatched').append(container_div)
+     });
 });

ec5fb4576f37cfd29110c77691caf2d6e3015969
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-05-15 20:14:59 +0000

    user storage

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
 watched = []
 
-$.getJSON('movies.json', function(data) {
-     $.each(data, function(i, f) {
-         console.log(f)
-		 container_div = $('<div class="container"></div>')
-		 if (f['cover']) {
-			container_div.append('<div class="image"><img src="'+ f['cover'] +'" width="50" height="75"></div>')
-		 }
-		 else {
-			container_div.append('<div class="image"><img src="android/comingsoon.jpg" width="50" height="75"></div>')
-		 }
-		 
-		 text_div = $('<div class="text"></div>')
+user_watched = localStorage.getItem('userWatched');
+if (user_watched) {
+	watched = user_watched
+}
 
-		 check_box = $('<input type="checkbox" id="' + f['id'] + '" name="movie_id" value="' + f['id'] + '">')
-
-		 check_box.change(function() {
-			if ($(this).is(':checked')) {
-				console.log(`${this.value} is checked`);
-				watched.push(this.value)
+function loadJson() {
+	$.getJSON('movies.json', function(data) {
+		$('#watched').empty()
+		$('#watched').append('<h1>Watched</h1>')
+		$('#unwatched').empty()
+		$('#unwatched').append('<h1>Unwatched</h1>')
+		$.each(data, function(i, f) {
+			// console.log(f)
+			container_div = $('<div class="container"></div>')
+			if (f['cover']) {
+			   container_div.append('<div class="image"><img src="'+ f['cover'] +'" width="50" height="75"></div>')
 			}
 			else {
-				console.log(`${this.value} is unchecked`);
+			   container_div.append('<div class="image"><img src="android/comingsoon.jpg" width="50" height="75"></div>')
 			}
-		 });
-	
-		 text_div.append(check_box)
-		 text_div.append('<label for="' + f['id'] + '">' + f['title'] + ' (' + f['year'] + ')</br>' + f['character']+'</label><br>')
+			
+			text_div = $('<div class="text"></div>')
+   
+			check_box = $('<input type="checkbox" id="' + f['id'] + '" name="movie_id" value="' + f['id'] + '">')
+   
+			check_box.change(function() {
+			   if ($(this).is(':checked')) {
+				   console.log(`${this.value} is checked`);
+				   watched.push(this.value)
+				   localStorage.setItem('userWatched', watched)
+				   loadJson()
+			   }
+			   else {
+				   console.log(`${this.value} is unchecked`);
+			   }
+			});
+	   
+			text_div.append(check_box)
+			text_div.append('<label for="' + f['id'] + '">' + f['title'] + ' (' + f['year'] + ')</br>' + f['character']+'</label><br>')
+   
+			container_div.append(text_div)
+			if (watched.includes(f['id'])) {
+			   $('#watched').append(container_div)
+			} else {
+			   $('#unwatched').append(container_div)
+			}
+		});
+   });
+}
 
-		 container_div.append(text_div)
-		 if (f['id'] in watched) {
-			$('#watched').append(container_div)
-		 } else {
-			$('#unwatched').append(container_div)
-		 }
-     });
-});
+
+loadJson()

d8a31de98529aa077d022af1076590cfd531f7bc
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-05-15 10:13:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 
-fetch('https://m.imdb.com/name/nm0000115/fullcredits', {            
-        method: 'GET',
-	mode: 'no-cors'
-    }).then(function (response) {
-	// The API call was successful!
-	return response.text();
-}).then(function (html) {
-	// This is the HTML from our response as a text string
-	console.log(html);
-}).catch(function (err) {
-	// There was an error
-	console.warn('Something went wrong.', err);
+$.get('https://m.imdb.com/name/nm0000115/fullcredits', function(html) { 
+	console.log(html); 
 });

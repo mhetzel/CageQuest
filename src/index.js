2dbc682de77ac4faaffa5701c10156f4dc545bc8
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-05-17 18:33:21 +0000

    extra details

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ if (user_watched) {
 	watched = user_watched.split(',');
 }
 
+displayExtraInfo = false;
 watched_count = watched.length;
+
 
 function loadJson() {
 	$.getJSON('movies.json', function(data) {
@@ -13,7 +15,7 @@ function loadJson() {
 		$('#unwatched').empty()
 		unwatched_count = data.length;
 		$.each(data, function(i, f) {
-			container_div = $('<div class="container"></div>')
+			let container_div = $('<div class="container"></div>')
 			if (f['cover']) {
 			   container_div.append('<div class="image"><img src="'+ f['cover'] +'" width="50" height="75"></div>')
 			}
@@ -21,9 +23,9 @@ function loadJson() {
 			   container_div.append('<div class="image"><img src="android/comingsoon.jpg" width="50" height="75"></div>')
 			}
 			
-			text_div = $('<div class="text"></div>')
+			let text_div = $('<div class="text"></div>')
    
-			check_box = $('<input type="checkbox" id="' + f['id'] + '" name="movie_id" value="' + f['id'] + '">');
+			let check_box = $('<input type="checkbox" id="' + f['id'] + '" name="movie_id" value="' + f['id'] + '">');
 			if (watched.includes(f['id'])) {
 			  check_box.prop('checked', true);
 			}
@@ -48,6 +50,31 @@ function loadJson() {
 	   
 			text_div.append(check_box)
 			text_div.append('<label for="' + f['id'] + '">' + f['title'] + ' (' + f['year'] + ')</br>' + f['character']+'</label><br>')
+			let expand_button = $('<button id="expand_'+f['id']+'"></button>');
+			let expandIcon = $('<i class="arrow right"></i>')
+			let expandedIcon = $('<i class="arrow down"></i>')
+
+			let extraDiv = $('<div id="extra_'+f['id']+'"></div>')
+			extraDiv.append('<div>'+f['genres'].join(", ")+'</div>')
+			extraDiv.append('<div>'+f['plot'].join(", ")+'</div>')
+
+			expand_button.append(expandIcon);
+			text_div.append(expand_button)
+
+			expand_button.click(function() {
+				if (displayExtraInfo) {
+					displayExtraInfo = false;
+					expandedIcon.remove()
+					expand_button.append(expandIcon);
+					extraDiv.remove()
+				  } else {
+					displayExtraInfo = true;
+					expandIcon.remove()
+					expand_button.append(expandedIcon);
+					text_div.append(extraDiv)
+				  }
+			})
+			
 			container_div.append(text_div)
 			if (watched.includes(f['id'])) {
 			   $('#watched').append(container_div)

e20af6bac1d76712091af662b37a09b377b8d5b2
mhetzel <34289010+mhetzel@users.noreply.github.com>
2025-07-22 08:16:24 -0500

    Update CageJson.py

--- CageJson.py
+++ CageJson.py
@@ -15,26 +15,26 @@ divs = soup.find_all(class_='.ipc-metadata-list-summary-item__tc')
 for div in divs:
     print(div.a.text)
 
-filmography_html = soup.find(id='filmo-head-actor')
+# filmography_html = soup.find(id='filmo-head-actor')
 
-all_films_html = filmography_html.find_next_sibling("div")
+# all_films_html = filmography_html.find_next_sibling("div")
 
-movies = []
+# movies = []
 
-for div in all_films_html.find_all('div'):
-    movie_id = div.b.a['href'].split('/')[2].strip('tt')
-    movie = ia.get_movie(movie_id)
-    print(div.b.a.text.strip('\n').strip())
-    movie_dict = {
-        'title' : div.b.a.text.strip('\n').strip(),
-        'year' : div.span.text.strip('\n').strip(),
-        'character' : div.br.next_sibling.strip('\n').strip(),
-        'plot' : movie.get('plot'),
-        'genres' : movie.get('genres'),
-        'cover' : movie.get('cover url'),
-        'id' : movie_id
-    }
-    movies.append(movie_dict)
+# for div in all_films_html.find_all('div'):
+#     movie_id = div.b.a['href'].split('/')[2].strip('tt')
+#     movie = ia.get_movie(movie_id)
+#     print(div.b.a.text.strip('\n').strip())
+#     movie_dict = {
+#         'title' : div.b.a.text.strip('\n').strip(),
+#         'year' : div.span.text.strip('\n').strip(),
+#         'character' : div.br.next_sibling.strip('\n').strip(),
+#         'plot' : movie.get('plot'),
+#         'genres' : movie.get('genres'),
+#         'cover' : movie.get('cover url'),
+#         'id' : movie_id
+#     }
+#     movies.append(movie_dict)
 # print(json.dumps(movies, indent=3))
 
 with open('movies.json', 'w') as f:

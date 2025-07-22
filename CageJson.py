b2307be78f5db2652274b92edc939238cf5788d1
mhetzel <34289010+mhetzel@users.noreply.github.com>
2025-07-22 08:56:04 -0500

    Update CageJson.py

--- CageJson.py
+++ CageJson.py
@@ -5,9 +5,9 @@ from imdb import Cinemagoer
 import imdb.helpers as helpers
 
 ia = Cinemagoer()
-cage = ia.get_person('0000115')
-print(cage['imdbID'])
-print(ia.get_person(cage['imdbID']).keys())
+cage = ia.get_person_filmography('0000115')
+print(cage)
+
 
 # html = requests.get('https://m.imdb.com/name/nm0000115/?showAllCredits=true', timeout=50)
 # # print(html.text)
